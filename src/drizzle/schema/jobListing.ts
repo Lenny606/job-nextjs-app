35674580918cdf83f77a4e5b88f32df3dd359637
Tomas Kravcik <kravcik@procorp.cz>
2025-06-19 21:15:36 +0200

    Extend job schema with additional enums and fields, update schema helpers with new enums and types

--- src/drizzle/schema/jobListing.ts
+++ src/drizzle/schema/jobListing.ts
@@ -1,21 +1,62 @@
-import {text, pgTable, integer, varchar, boolean, uuid} from "drizzle-orm/pg-core";
-import {createdAt, updatedAt, id, wageIntervalEnum} from "@/drizzle/schemaHelper";
+import {text, pgTable, integer, varchar, boolean, uuid, timestamp, index} from "drizzle-orm/pg-core";
+import {
+    createdAt,
+    updatedAt,
+    id,
+    wageIntervalEnum,
+    locationRequirementEnum,
+    statusEnum,
+    experienceEnum, typeEnum
+} from "@/drizzle/schemaHelper";
 import {OrganizationTable} from "@/drizzle/schema/organization";
 
 
+/**
+ * Represents the structure of the "job_listings" table in the database.
+ *
+ * This table stores information about job listings including details such as title,
+ * description, associated organization, wage, location, and other metadata.
+ *
+ * Fields:
+ * - id: The primary identifier for a job listing.
+ * - title: The title of the job listing. This is a required field.
+ * - description: Detailed information about the job listing. This is a required field.
+ * - organizationId: The associated organization for the job listing. This is required and references the "OrganizationTable" id, with cascade delete behavior.
+ * - wage: The wage amount for the job listing, if specified.
+ * - wageInterval: The frequency at which the wage is paid (e.g., hourly, daily).
+ * - stateAbbreviation: The state abbreviation associated with the job's location.
+ * - city: The city associated with the job's location.
+ * - isFeatured: Indicates whether the job listing is featured. Defaults to false.
+ * - locationRequirement: Specifies the location requirement (e.g., remote, on-site). This is a required field.
+ * - status: The current status of the job listing (e.g., draft, published). Defaults to "draft".
+ * - experience: Represents the experience level requirement for the job listing. This is a required field.
+ * - type: The type or category of the job listing. This is a required field.
+ * - published: The timestamp of when the job was published, includes timezone information.
+ * - createdAt: The timestamp of when the job listing was created.
+ * - updatedAt: The timestamp of the last update to the job listing.
+ *
+ * Indexes:
+ * - An index is applied on the "stateAbbreviation" field.
+ */
 export const JobListingTable = pgTable("job_listings", {
-    id,
-    title: varchar().notNull(),
-    description: text().notNull(),
-    organizationId: uuid().references(() => OrganizationTable.id, {
-        onDelete: "cascade"
-    }).notNull(),
-    wage: integer(),
-    wageInterval: wageIntervalEnum(),
-    stateAbbreviation: varchar(),
-    city: varchar(),
-    isFeatured: boolean().notNull().default(false),
-
-    createdAt,
-    updatedAt
-})
+        id,
+        title: varchar().notNull(),
+        description: text().notNull(),
+        organizationId: uuid().references(() => OrganizationTable.id, {
+            onDelete: "cascade"
+        }).notNull(),
+        wage: integer(),
+        wageInterval: wageIntervalEnum(),
+        stateAbbreviation: varchar(),
+        city: varchar(),
+        isFeatured: boolean().notNull().default(false),
+        locationRequirement: locationRequirementEnum().notNull(),
+        status: statusEnum().notNull().default("draft"),
+        experience: experienceEnum().notNull(),
+        type: typeEnum().notNull(),
+        published: timestamp({withTimezone: true}),
+        createdAt,
+        updatedAt
+    },
+    table => [index().on(table.stateAbbreviation)]
+)
--- src/drizzle/schemaHelper.ts
+++ src/drizzle/schemaHelper.ts
@@ -13,7 +13,19 @@ export const id = uuid().primaryKey().defaultRandom()
 
 // ------------- ENUMS----------
 export const wageIntervals = ["hourly", "daily", "weekly", "monthly", "yearly"] as const;
+export const locationRequirements = ["office", "remote", "hybrid"] as const;
+export const statuses = ["draft", "published", "delisted"] as const;
+export const experiences = ["junior", "medior", "senior"] as const;
+export const types = ["full-time", "part-time", "internship"] as const;
 export const wageIntervalEnum = pgEnum('wage_intervals', wageIntervals)
+export const locationRequirementEnum = pgEnum('location_requirements', locationRequirements)
+export const statusEnum = pgEnum('statuses', statuses)
+export const experienceEnum = pgEnum('experiences', experiences)
+export const typeEnum = pgEnum('types', types)
 
 // ------------- TYPES----------
 export type WageInterval = (typeof wageIntervals)[number];
+export type LocationRequirement = (typeof locationRequirements)[number];
+export type Status = (typeof statuses)[number];
+export type Experience = (typeof experiences)[number];
+export type Type = (typeof types)[number];
