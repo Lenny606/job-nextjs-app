b14301e75c14d8748f078f0512f2b4f975757a43
Tomas Kravcik <kravcik@procorp.cz>
2025-06-19 22:11:34 +0200

    Add Drizzle CLI scripts to `package.json` and update Drizzle config for schema path changes

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,9 @@
-import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 import {env} from "@/data/env/server";
 
 export default defineConfig({
     out: './src/drizzle/migrations',
-    schema: './src/drizzle/schema/schema.ts',
+    schema: './src/drizzle/schema.ts',
     dialect: 'postgresql',
     dbCredentials: {
         url: env.DATABASE_URL!,
