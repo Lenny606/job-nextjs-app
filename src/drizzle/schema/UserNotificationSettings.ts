4780f2f816c41b4f3f284df3e18572bf758139f0
Tomas Kravcik <kravcik@procorp.cz>
2025-06-19 21:39:20 +0200

    Add `UserNotificationSettings` schema with user reference, notification preferences, and timestamps

---
+++ src/drizzle/schema/UserNotificationSettings.ts
@@ -0,0 +1,23 @@
+import {
+    pgTable,
+    uuid,
+    boolean, varchar
+} from "drizzle-orm/pg-core";
+import {
+    createdAt,
+    updatedAt,
+    id
+} from "@/drizzle/schemaHelper";
+import {UserTable} from "@/drizzle/schema/user";
+
+export const UserNotificationSettings = pgTable("user_notification_settings", {
+        id,
+        userId: uuid().references(() => UserTable.id, {
+            onDelete: "cascade"
+        }).notNull().primaryKey(),
+        newJobEmailNotification: boolean().notNull().default(false),
+        aiPrompt: varchar(),
+        createdAt,
+        updatedAt
+    }
+)
