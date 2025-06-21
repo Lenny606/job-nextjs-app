0c7413a8e7c989ff05cad2a69d7899bd9329dcf7
Tomas Kravcik <kravcik@procorp.cz>
2025-06-21 14:50:09 +0200

    Integrate Sidebar component into the homepage and update dependencies for UI enhancements.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,34 @@
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem,
+    SidebarProvider,
+    SidebarTrigger
+} from "@/components/ui/sidebar";
 
 
 export default function Home() {
-  return (
-   <h1>job</h1>
-  );
+    return (
+        <SidebarProvider className={'overflow-y-hidden'}>
+            <Sidebar className={'overflow-hidden'}
+                     collapsible={'icon'}>
+                <SidebarHeader className={"flex-row"}>
+                    <SidebarTrigger className={''}></SidebarTrigger>
+                    <span className={'text-xl text-wrap'}>JOB APP</span>
+                </SidebarHeader>
+                <SidebarContent>
+
+                </SidebarContent>
+                <SidebarFooter>
+                    <SidebarMenu>
+                        <SidebarMenuItem>
+                            <SidebarMenuButton>Button footer</SidebarMenuButton>
+                        </SidebarMenuItem>
+                    </SidebarMenu>
+                </SidebarFooter>
+            </Sidebar>
+            <main className={'flex-1'}>MAIN CONTENT</main>
+        </SidebarProvider>
+    );
 }
