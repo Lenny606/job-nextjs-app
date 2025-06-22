0d9e28f5ead42fe5fbcb76f3503338bf85e2f4bd
Tomas Kravcik <kravcik@procorp.cz>
2025-06-22 10:34:07 +0200

    Refactor `globals.css` for improved dark mode support and update `ClerkLayout` for responsive alignment adjustments.

--- src/app/(clerk)/layout.tsx
+++ src/app/(clerk)/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function ClerkLayout({children}: { children: React.ReactNode }) {
     return (
-        <div className={"flex h-screen w-screen items-center"}>
+        <div className={"flex flex-row h-screen w-screen justify-center items-center"}>
             <div>{children}</div>
         </div>
     )
